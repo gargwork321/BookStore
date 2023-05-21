27e197072eb5bdf87c2fb9de7482f3f433535b09
Vivek  Garg <vivekgarg@Viveks-Air.STELFIBER>
2023-05-21 12:06:57 +0530

    navigation seprated

--- App.tsx
+++ App.tsx
@@ -6,36 +6,10 @@
  */
 
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Screens from './src/constants/Screens';
-import HomeScreen from './src/screens/HomeScreen';
-import BookDetailScreen from './src/screens/BookDetailScreen';
-import ListingScreen from './src/screens/ListingScreen';
+import AppNavigation from './src/navigation/appNavigation';
 
-const Stack = createNativeStackNavigator();
 function App(): JSX.Element {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name={Screens.HOME}
-          component={HomeScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name={Screens.BOOK_DETAILS}
-          component={BookDetailScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name={Screens.LISTING}
-          component={ListingScreen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <AppNavigation />;
 }
 
 export default App;
---
+++ src/navigation/appNavigation.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Screens from '../constants/Screens';
+import HomeScreen from '../screens/HomeScreen';
+import BookDetailScreen from '../screens/BookDetailScreen';
+import ListingScreen from '../screens/ListingScreen';
+
+const Stack = createNativeStackNavigator();
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={Screens.HOME}
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name={Screens.BOOK_DETAILS}
+          component={BookDetailScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name={Screens.LISTING}
+          component={ListingScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
